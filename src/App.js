43717d57eb85423115da20f4ca37b776679c22c8
Jaroslaw Laba <jarek.laba@gmail.com>
2023-05-16 23:05:47 +0200

    started building components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import './style.css';
 
 function App() {
+  const appTitle = "That's a fact";
+
   return (
-    <header class="header flex">
-      <div class="logo flex">
-        <img src="/logo.png" alt="Logo" />
-        <h1>That's a fact</h1>
-      </div>
-      <button class="btn btn-large btn-share">Share a fact</button>
-    </header>
+    <>
+      {/* HEADER will get its own component */}
+      <header className="header flex">
+        <div className="logo flex">
+          <img src="/logo.png" alt="Logo" />
+          <h1>{appTitle}</h1>
+        </div>
+        <button className="btn btn-large btn-share">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main className="main-content grid">
+        <CategoryFilter />
+        <FactsList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form flex">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactsList() {
+  return <section class="section__facts">Facts list</section>;
 }
 
 export default App;

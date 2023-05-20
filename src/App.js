945a07fb852f414ebe17feb3267697a5e50b595a
Jaroslaw Laba <jarek.laba@gmail.com>
2023-05-20 15:45:13 +0200

    divided an app into separate components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
+import { useState } from 'react';
 import './style.css';
 
+const initialFacts = [
+  {
+    id: 1,
+    text: 'React is being developed by Meta (formerly facebook)',
+    source: 'https://opensource.fb.com/',
+    category: 'technology',
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: 'Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%',
+    source:
+      'https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids',
+    category: 'society',
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: 'Lisbon is the capital of Portugal',
+    source: 'https://en.wikipedia.org/wiki/Lisbon',
+    category: 'society',
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
 function App() {
-  const appTitle = "That's a fact";
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <>
-      {/* HEADER will get its own component */}
-      <header className="header flex">
-        <div className="logo flex">
-          <img src="/logo.png" alt="Logo" />
-          <h1>{appTitle}</h1>
-        </div>
-        <button className="btn btn-large btn-share">Share a fact</button>
-      </header>
+      <Header showForm={showForm} setShowForm={setShowForm} />
 
-      <NewFactForm />
+      {showForm ? <NewFactForm /> : null}
 
       <main className="main-content grid">
         <CategoryFilter />
@@ -24,16 +52,141 @@ function App() {
   );
 }
 
+function Header({ showForm, setShowForm }) {
+  const appTitle = "That's a fact";
+
+  return (
+    <header className="header flex">
+      <div className="logo flex">
+        <img src="/logo.png" alt="Logo" />
+        <h1>{appTitle}</h1>
+      </div>
+      <button
+        className="btn btn-large btn-share"
+        onClick={() => setShowForm((show) => !show)}
+      >
+        {showForm ? 'Close' : 'Share a fact'}
+      </button>
+    </header>
+  );
+}
+
+const CATEGORIES = [
+  { name: 'technology', color: '#3b82f6' },
+  { name: 'science', color: '#16a34a' },
+  { name: 'finance', color: '#ef4444' },
+  { name: 'society', color: '#eab308' },
+  { name: 'entertainment', color: '#db2777' },
+  { name: 'health', color: '#14b8a6' },
+  { name: 'history', color: '#f97316' },
+  { name: 'news', color: '#8b5cf6' },
+];
+
 function NewFactForm() {
-  return <form className="fact-form flex">Fact form</form>;
+  const [text, setText] = useState('');
+  const [source, setSource] = useState('');
+  const [category, setCategory] = useState('');
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text, source, category);
+  }
+
+  return (
+    <form className="fact-form flex" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        type="text"
+        placeholder="Trustworthy source"
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose category</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
 }
 
 function CategoryFilter() {
-  return <aside>Category filter</aside>;
+  return (
+    <aside>
+      <ul className="categories">
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li key={cat.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 function FactsList() {
-  return <section class="section__facts">Facts list</section>;
+  // TEMPORARY
+  const facts = initialFacts;
+
+  return (
+    <section className="section__facts">
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <Fact key={fact.id} factObj={fact} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+// { factObj } - destructuring
+// the same as factObj = props.factObj
+// the same as { factObj } = props
+function Fact({ factObj }) {
+  return (
+    <li className="fact">
+      <p>
+        {factObj.text}
+        <a className="source" href={factObj.source} target="_blank">
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find(
+            (cat) => cat.name === factObj.category
+          ).color,
+        }}
+      >
+        {factObj.category}
+      </span>
+      <div className="vote-buttons flex">
+        <button>👍 {factObj.votesInteresting}</button>
+        <button>🤯 {factObj.votesMindblowing}</button>
+        <button>⛔️ {factObj.votesFalse}</button>
+      </div>
+    </li>
+  );
 }
 
 export default App;
